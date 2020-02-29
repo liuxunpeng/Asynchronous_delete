6fe56165304115d65ceaec6fd43db4301006d42b
鹏鹏君 <18610907614@163.com>
2020-02-29 17:28:33 +0800

    入口

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"video_server/Scheduler/taskrunner"
+)
+
+func RegisterHandlers()*httprouter.Router{
+	router:=httprouter.New()
+	router.GET("/video-delete-record/:vid-id",vidDelRecHandler)
+	return router
+}
+func main(){
+	go taskrunner.Start()
+	r := RegisterHandlers()
+	http.ListenAndServe(":9001", r)
+}
